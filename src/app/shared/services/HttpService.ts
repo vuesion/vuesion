213170b413a30a2b63327231a2b0de85ddaf140f
Johannes Werner <johannes.werner@bcgdv.com>
2018-04-01 19:10:35 +0200

    refactor(http-service): make it work for i18n; update fresh token logic

--- src/app/shared/services/HttpService.ts
+++ src/app/shared/services/HttpService.ts
@@ -1,25 +1,19 @@
 import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios';
 import { store }                                                from '../../store';
-/**
- * TODO: import store to access auth token
- * import { store } from '../../store';
- * @type {number}
- */
-
-const RETRY_COUNT: number = 3;
 
 const isExternalUrl = (url: string): boolean => {
-  return url.indexOf('://') > -1;
+  return url.indexOf('://') > -1 && url.indexOf('i18n') === -1;
 };
-let retries: number = RETRY_COUNT;
+
+export const HttpService = axios.create();
 
 /* istanbul ignore next */
-axios.interceptors.request.use(
+HttpService.interceptors.request.use(
   (config: AxiosRequestConfig) => {
     // TODO: get token from store e.g. const token: string = store.state.auth.accessToken;
     const token: string = 'accessToken';
-    const isExternal: boolean = isExternalUrl(config.url);
     const baseUrl: string = store.state.app.config.api.baseUrl;
+    const isExternal: boolean = isExternalUrl(config.url);
 
     if (token && !isExternal && !config.headers.Authorization) {
       config.headers.Authorization = `Bearer ${token}`;
@@ -36,38 +30,36 @@ axios.interceptors.request.use(
   },
 );
 
-/* istanbul ignore next */
-axios.interceptors.response.use(
+HttpService.interceptors.response.use(
   (response: AxiosResponse) => {
     return response;
   },
   (error: AxiosError) => {
-    const originalRequest = error.config;
+    const originalRequest: any = error.config;
     /**
      * TODO: add condition for refreshing the token
      * const shouldRefresh: boolean = store.state.auth.accessToken !== null &&
      * (error.response.status === 401 || error.response.status === 403) &&
-     * retries > 0;
+     * !originalRequest._retry;
      */
 
     const shouldRefresh: boolean = false;
 
     if (shouldRefresh) {
-      retries = retries - 1;
+      originalRequest._retry = true;
+
       /**
        * TODO: return function that refreshes the token
        * return store.dispatch('refreshToken')
        * .then(() => {
-       *   return axios(originalRequest);
+       *     if (originalRequest.headers.Authorization && originalRequest.headers.Authorization.indexOf('Bearer') > -1) {
+       *       originalRequest.headers.Authorization = `Bearer ${store.state.auth.accessToken}`;
+       *     }
+       *
+       *     return Promise.resolve(HttpService(originalRequest));
        * });
        */
     }
 
-    if (retries <= 0) {
-      retries = RETRY_COUNT;
-    }
-
     return Promise.reject(error);
   });
-
-export const HttpService = axios;
