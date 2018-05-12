18d82d605aa05fc684ac946ed4b0a90a51d2ebec
Johannes Werner <johannes.werner86@gmail.com>
2018-05-12 19:47:00 +0200

    feat: add client-side error logging (#128)
    closes #124

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,11 +1,10 @@
-import * as Express          from 'express';
-import { Request, Response } from 'express';
-import { Logger }            from './utils/Logger';
-import { serve }             from './utils/Utils';
-import { applyMiddlewares }  from './middlewares';
-import { StaticRoutes }      from './routes/StaticRoutes';
-import { CounterRoutes }     from './routes/CounterRoutes';
-import { SSRRoutes }         from './routes/SSRRoutes';
+import * as Express         from 'express';
+import { Logger }           from './utils/Logger';
+import { applyMiddlewares } from './middlewares';
+import { DemoRoutes }       from './routes/DemoRoutes';
+import { CounterRoutes }    from './routes/CounterRoutes';
+import { StaticRoutes }     from './routes/StaticRoutes';
+import { SSRRoutes }        from './routes/SSRRoutes';
 
 const app: Express.Application = Express();
 const port: string = process.env.PORT || '3000';
@@ -15,37 +14,16 @@ app.disable('x-powered-by');
 applyMiddlewares(app);
 
 /**
- * http -> https redirect for heroku
- * TODO: remove if you're not running the app on heroku
+ * routes to demonstrate the possibilities of the vue-starter
+ * can be removed if you don't need them in your application
  */
-app.get('*', (req: Request, res: Response, next: any) => {
-  const host: string = req.headers.host || 'localhost:3000';
-  const redirect: string = `https://${host}` + req.url;
-
-  if (req.headers['x-forwarded-proto'] && req.headers['x-forwarded-proto'] !== 'https') {
-    res.redirect(redirect);
-  } else {
-    next();
-  }
-});
-
-/**
- * storybook
- * TODO: enable this route only in non production environments
- * if (!isProd) {
- *    app.use('/storybook', serve('../../storybook-static', true));
- * }
- */
-app.use('/storybook', serve('../../storybook-static'));
-
-/**
- * docs
- * TODO: remove if you don't maintain a documentation
- */
-app.use('/docs', serve('../docs'));
-
-StaticRoutes(app);
+DemoRoutes(app);
 CounterRoutes(app);
+
+/**
+ * core routes, don't delete these
+ */
+StaticRoutes(app);
 SSRRoutes(app);
 
 app.listen(port, () => {
