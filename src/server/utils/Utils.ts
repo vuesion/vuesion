f56c0f8e260d88b48e0df2690cabd3eba6052146
Johannes Werner <johannes.werner@bcgdv.com>
2018-06-09 17:43:43 +0200

    fix(performance): increase http cache time
    closes #149

--- src/server/utils/Utils.ts
+++ src/server/utils/Utils.ts
@@ -7,7 +7,7 @@ export const isProd: boolean = process.env.NODE_ENV === 'production';
 export const resolve = (file: string): string => path.resolve(__dirname, file);
 
 export const serve = (servePath: string, cache: boolean = true): Handler => Express.static(resolve(servePath), {
-  maxAge:     cache && isProd ? '24h' : 0,
+  maxAge:     cache && isProd ? '4w' : 0,
   setHeaders: (res: Response) => {
     res.setHeader('X-Content-Type-Options', 'nosniff');
   },
