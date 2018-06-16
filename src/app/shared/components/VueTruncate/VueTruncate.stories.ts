27a69d02efb1972f3d746e8274b3df851e6f470a
Johannes Werner <johannes.werner86@gmail.com>
2018-06-16 10:58:12 +0200

    feat(components): add text truncate component (#165)
    closes #162

---
+++ src/app/shared/components/VueTruncate/VueTruncate.stories.ts
@@ -0,0 +1,47 @@
+import { storiesOf } from '@storybook/vue';
+import VueInfoAddon  from 'storybook-addon-vue-info';
+import VueTruncate  from './VueTruncate.vue';
+import { i18n }      from '../../plugins/i18n/i18n';
+
+const story = (storiesOf('VueTruncate', module) as any);
+
+story.addDecorator(VueInfoAddon);
+
+story.add('Default', () => ({
+  i18n,
+  components: { VueTruncate },
+  template:   `<vue-truncate>
+  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et<br/>
+  dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet<br/>
+  clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,<br/>
+  consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,<br/>
+  sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,<br/>
+  no sea takimata sanctus est Lorem ipsum dolor sit amet.
+</vue-truncate>`,
+}));
+
+story.add('Without truncation', () => ({
+  i18n,
+  components: { VueTruncate },
+  template:   `<vue-truncate :lines="6">
+  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et<br/>
+  dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet<br/>
+  clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,<br/>
+  consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,<br/>
+  sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,<br/>
+  no sea takimata sanctus est Lorem ipsum dolor sit amet.
+</vue-truncate>`,
+}));
+
+story.add('Lines and Duration', () => ({
+  i18n,
+  components: { VueTruncate },
+  template:   `<vue-truncate :lines="2" :duration="500">
+  Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et<br/>
+  dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet<br/>
+  clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet,<br/>
+  consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,<br/>
+  sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,<br/>
+  no sea takimata sanctus est Lorem ipsum dolor sit amet.
+</vue-truncate>`,
+}));
