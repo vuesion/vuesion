33707ecc9eb9ec8e083a7cd8b15e9e988302988e
xMartin <xmartin@siarp.de>
2018-07-22 19:59:39 +0200

    feat(vue-navigation-progress): always reach 100% and fade out [skip ci]

--- src/app/shared/components/VueNavigationProgress/VueNavigationProgress.vue
+++ src/app/shared/components/VueNavigationProgress/VueNavigationProgress.vue
@@ -23,6 +23,7 @@
     methods: {
       startAnimation() {
         this.interval = setInterval(() => {
+          this.show = true;
           if (this.percent <= 50) {
             this.percent += randomInt(30, 40);
           } else if (this.percent <= 70) {
@@ -36,8 +37,17 @@
         }, 100);
       },
       stopAnimation() {
+        const transitionDuration = 250;
+        const widthTransitionDuration = transitionDuration * 2;
         clearInterval(this.interval);
         this.interval = null;
+        this.percent = 100;
+        setTimeout(() => {
+          this.show = false;
+          setTimeout(() => {
+            this.percent = 0;
+          }, transitionDuration);
+        }, widthTransitionDuration);
       },
     },
     watch:   {
@@ -45,12 +55,9 @@
         immediate: true,
         handler(isNavigating: boolean) {
           if (isNavigating) {
-            this.show = true;
             this.startAnimation();
           } else {
-            this.show = false;
             this.stopAnimation();
-            this.percent = 0;
           }
         },
       },
@@ -69,7 +76,7 @@
     z-index:    $nav-bar-index + 5;
     height:     2px;
     @include background-gradient($brand-accent, lighten($brand-accent, 30%), 'horizontal');
-    transition: width $transition-duration * 2 linear;
+    transition: width $transition-duration * 2 linear, opacity $transition-duration;
     opacity:    0;
   }
 
