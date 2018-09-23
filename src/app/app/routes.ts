71ab4ceb528cc785c3623356f866fe4ce49e33a9
xMartin <xmartin@siarp.de>
2018-09-23 14:47:05 +0200

    chore: add comment to catch-all route

--- src/app/app/routes.ts
+++ src/app/app/routes.ts
@@ -26,6 +26,9 @@ export const AppRoutes: RouteConfig[] = [
     meta:      { requiresAuth: true },
   },
 
+  // catch-all route
+  // shows 404 page and also makes server respond with HTTP status code 404
+  // make sure to also adjust `src/server/isomorphic` in case you implement a more complex behavior here
   {
     path:      '*',
     component: () => import(/* webpackChunkName: "notFound" */ './NotFound/NotFound.vue').then(m => m.default),
