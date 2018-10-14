728d6b059a3c53691f29a45d28f3df0a760efed6
Johannes Werner <johannes.werner@bcgdv.com>
2018-10-14 12:27:01 +0200

    chore: update vue-starter presentation

--- src/app/shared/components/VueGrid/VueGrid.vue
+++ src/app/shared/components/VueGrid/VueGrid.vue
@@ -17,26 +17,26 @@
     max-width: 425px;
     margin:    0 auto;
     position:  relative;
-    padding:   0 $screen-phone-gutter * 0.5;
+    padding:   0 $screen-phone-gutter;
 
     @include mediaMin(tabletPortrait) {
       max-width: $screen-tablet-portrait;
-      padding:   0 $screen-tablet-portrait-gutter * 0.5;
+      padding:   0 $screen-tablet-portrait-gutter;
     }
 
     @include mediaMin(tabletLandscape) {
       max-width: $screen-tablet-landscape;
-      padding:   0 $screen-tablet-landscape-gutter * 0.5;
+      padding:   0 $screen-tablet-landscape-gutter;
     }
 
     @include mediaMin(smallDesktop) {
       max-width: $screen-small-desktop;
-      padding:   0 $screen-small-desktop-gutter * 0.5;
+      padding:   0 $screen-small-desktop-gutter;
     }
 
     @include mediaMin(largeDesktop) {
       max-width: $screen-large-desktop;
-      padding:   0 $screen-large-desktop-gutter * 0.5;
+      padding:   0 $screen-large-desktop-gutter;
     }
   }
 </style>
