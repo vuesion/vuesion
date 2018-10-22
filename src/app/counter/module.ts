eef3bee533edce2559ab4941f6f50df65ed9d82d
xMartin <xmartin@siarp.de>
2018-10-22 14:50:07 +0200

    fix: module typings

--- src/app/counter/module.ts
+++ src/app/counter/module.ts
@@ -1,10 +1,11 @@
 import { Module }                             from 'vuex';
 import { CounterDefaultState, ICounterState } from './state';
+import { IState }                             from '../state';
 import { CounterActions }                     from './actions';
 import { CounterGetters }                     from './getters';
 import { CounterMutations }                   from './mutations';
 
-export const CounterModule: Module<ICounterState, ICounterState> = {
+export const CounterModule: Module<ICounterState, IState> = {
   namespaced: true,
   actions:    {
     ...CounterActions,
