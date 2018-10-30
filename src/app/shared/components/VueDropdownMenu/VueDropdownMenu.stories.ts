361f5c2796f3496f11780b25b04a8d7f9b07dd1f
Johannes Werner <johannes.werner86@gmail.com>
2018-10-30 19:51:58 +0100

    feat(vue-dropdown-menu): add separator option (#279)

--- src/app/shared/components/VueDropdownMenu/VueDropdownMenu.stories.ts
+++ src/app/shared/components/VueDropdownMenu/VueDropdownMenu.stories.ts
@@ -9,10 +9,12 @@ story.addDecorator(VueInfoAddon);
 
 story.add('Default', () => ({
   components: { VueDropdownMenu },
-  template:   `<div>This is a dropdown menu within <vue-dropdown-menu @click="action"
-:options="[{ label: 'Save', value: 'save' }, { label: 'Open', value: 'open' }, { label: 'Delete', value: 'delete' }]">
+  template:   `<div>This is a dropdown menu within
+<vue-dropdown-menu @click="action"
+:options="[{ label: 'Save', value: 'save' }, { label: '', value: 'separator' }, { label: 'Open', value: 'open' }, { label: 'Delete', value: 'delete' }]">
 File
-</vue-dropdown-menu> a normal text. It should perfectly fit.</div>`,
+</vue-dropdown-menu>
+a normal text. It should perfectly fit.</div>`,
   methods:    {
     action: action('@onClick'),
   },
