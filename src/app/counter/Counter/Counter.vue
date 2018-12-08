f9f9798d417f88ea9249f54c1e33ed8c5638dfe3
Johannes Werner <johannes.werner86@gmail.com>
2018-12-08 17:32:49 +0100

    chore: update dependencies

--- src/app/counter/Counter/Counter.vue
+++ src/app/counter/Counter/Counter.vue
@@ -5,12 +5,12 @@
         <vue-grid-item fill> <vue-headline level="1">Counter</vue-headline> </vue-grid-item>
 
         <vue-grid-item fill>
-          <vue-button variant="secondary" :loading="incrementPending" @click="increment">Increment +1 </vue-button>
+          <vue-button color="secondary" :loading="incrementPending" @click="increment">Increment +1 </vue-button>
           <br />
           <br />
         </vue-grid-item>
         <vue-grid-item fill>
-          <vue-button variant="primary" :loading="decrementPending" @click="decrement">Decrement -1 </vue-button>
+          <vue-button color="primary" :loading="decrementPending" @click="decrement">Decrement -1 </vue-button>
           <vue-headline level="3">Count is {{ count }}</vue-headline>
         </vue-grid-item>
       </vue-grid-row>
