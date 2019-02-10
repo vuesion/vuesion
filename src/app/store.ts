0f6df31204c54e01d63d159516e9c9ba1888f07d
Johannes Werner <johannes.werner86@gmail.com>
2019-02-10 19:47:53 +0100

    chore: update dependencies

--- src/app/store.ts
+++ src/app/store.ts
@@ -3,28 +3,12 @@ import Vuex, { Module, Store } from 'vuex';
 import merge from 'deepmerge';
 import { DefaultState, IState } from './state';
 import { VuexPersist } from 'vue-starter-vuex-persist';
-import { PersistLocalStorage } from 'vue-starter-vuex-persist/dist/PersistLocalStorage';
 import { PersistCookieStorage } from 'vue-starter-vuex-persist/dist/PersistCookieStorage';
 import { AppModule } from './app/module';
 
 Vue.use(Vuex);
 
 const state: IState = (CLIENT && window.__INITIAL_STATE__) || DefaultState;
-
-/* istanbul ignore next */
-const beforePersistLocalStorage = (localState: IState): IState => {
-  /**
-   * because the counter module is loaded on demand
-   * we have to check if it exists before we delete
-   * some state attributes
-   */
-  if (localState.counter) {
-    delete localState.counter.incrementPending;
-    delete localState.counter.decrementPending;
-  }
-
-  return localState;
-};
 
 /* istanbul ignore next */
 const beforePersistCookieStorage = (localState: IState): IState => {
@@ -39,7 +23,6 @@ export const store: Store<IState> = new Vuex.Store({
   state,
   plugins: [
     VuexPersist([
-      new PersistLocalStorage(['counter'], beforePersistLocalStorage),
       new PersistCookieStorage(['app'], {
         cookieOptions: {
           expires: 365,
@@ -54,11 +37,6 @@ export const registerModule = (moduleName: string, module: Module<any, any>) => 
   const moduleIsRegistered: boolean = (store as any)._modules.root._children[moduleName] !== undefined;
   const stateExists: boolean = store.state[moduleName] !== undefined;
 
-  /**
-   * merge existing state hydrated from vuex persist
-   * with module default state
-   * TODO: revisit this solution and figure out a better way to use it with vuex-persist
-   */
   if (stateExists) {
     module.state = merge(module.state, store.state[moduleName], {
       clone: false,
