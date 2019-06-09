163c95d8fe33dbe2fb337ef125e69028367d03f8
Johannes Werner <johannes.werner86@gmail.com>
2019-06-10 00:24:08 +0200

    fix(storybook): merge webpack resolve attributes

--- .vuesion/storybook/webpack.config.js
+++ .vuesion/storybook/webpack.config.js
@@ -2,7 +2,9 @@ const webpack = require('webpack');
 const base = require('../../node_modules/@vuesion/service/dist/webpack/config/base').default;
 
 module.exports = ({ config, mode }) => {
-  config.resolve = base.resolve;
+  config.resolve.extensions = [...config.resolve.extensions, ...base.resolve.extensions];
+  config.resolve.modules = [...config.resolve.modules, ...base.resolve.modules];
+  config.resolve.alias = { ...config.resolve.alias, ...base.resolve.alias };
   config.module.rules = base.module.rules;
 
   config.plugins.push(new webpack.DefinePlugin({ PRODUCTION: false, DEVELOPMENT: true, CLIENT: true, TEST: true }));
