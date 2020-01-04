1dbf0dc5be5ab380e4d6bfd2da1f5727171bc6d4
Johannes Werner <johannes.werner86@gmail.com>
2020-01-04 22:31:16 +0100

    refactor(prefetch): call App.prefetch first, then AuthGuard (#493)
    closes #350

--- src/server/utils/Logger.ts
+++ src/server/utils/Logger.ts
@@ -2,19 +2,6 @@ import * as winston from 'winston';
 
 export const Logger: winston.Logger = winston.createLogger({
   transports: [
-    new winston.transports.File({
-      filename: 'logs/error.log',
-      level: 'error',
-      maxFiles: 5,
-      maxsize: 10485760,
-      format: winston.format.combine(winston.format.splat(), winston.format.json()),
-    }),
-    new winston.transports.File({
-      filename: 'logs/all.log',
-      maxFiles: 5,
-      maxsize: 10485760,
-      format: winston.format.combine(winston.format.splat(), winston.format.json()),
-    }),
     new winston.transports.Console({
       level: 'debug',
       handleExceptions: true,
