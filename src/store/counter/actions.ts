10784c4dd08335ff75f7c45ef6f975c1abf6af66
Thomas Misikoff <tmisikoff@gmail.com>
2020-06-13 21:57:21 -0500

    feat: got more components and tests working

--- src/store/counter/actions.ts
+++ src/store/counter/actions.ts
@@ -1,47 +1,45 @@
 import { ActionContext } from 'vuex';
 import { ICounterState } from './state';
 // import { IState } from '@/app/state';
-// import { HttpService } from '@shared/services/HttpService/HttpService';
+import { HttpService } from '@/components/services/HttpService/HttpService';
 
 export interface ICounterResponse {
   count: number;
 }
 
-// export interface ICounterActions {
-//   increment(context: ActionContext<ICounterState, IState>): Promise<any>;
+export interface ICounterActions {
+  increment(context: ActionContext<ICounterState, ICounterState>): Promise<any>;
 
-//   decrement(context: ActionContext<ICounterState, IState>): Promise<any>;
-// }
+  decrement(context: ActionContext<ICounterState, ICounterState>): Promise<any>;
+}
 
-// export const CounterActions: ICounterActions = {
-//   async increment({ commit, state }: ActionContext<ICounterState, IState>) {
-//     commit('SET_INCREMENT_PENDING', true);
+export const CounterActions: ICounterActions = {
+  async increment({ commit, state }: ActionContext<ICounterState, ICounterState>) {
+    commit('SET_INCREMENT_PENDING', true);
 
-//     try {
-//       const res = await HttpService.put<ICounterResponse>('/counter/increment', { count: state.count });
+    try {
+      const res = await HttpService.put<ICounterResponse>('/counter/increment', { count: state.count });
 
-//       commit('SET_COUNT', res.data.count);
-//       commit('SET_INCREMENT_PENDING', false);
-//     } catch (e) {
-//       commit('SET_INCREMENT_PENDING', false);
-//       throw new Error(e);
-//     }
-//   },
-//   async decrement({ commit, state }: ActionContext<ICounterState, IState>) {
-//     commit('SET_DECREMENT_PENDING', true);
+      commit('SET_COUNT', res.data.count);
+      commit('SET_INCREMENT_PENDING', false);
+    } catch (e) {
+      commit('SET_INCREMENT_PENDING', false);
+      throw new Error(e);
+    }
+  },
+  async decrement({ commit, state }: ActionContext<ICounterState, ICounterState>) {
+    commit('SET_DECREMENT_PENDING', true);
 
-//     try {
-//       const res = await HttpService.put<ICounterResponse>('/counter/decrement', { count: state.count });
+    try {
+      const res = await HttpService.put<ICounterResponse>('/counter/decrement', { count: state.count });
 
-//       commit('SET_COUNT', res.data.count);
-//       commit('SET_DECREMENT_PENDING', false);
-//     } catch (e) {
-//       commit('SET_DECREMENT_PENDING', false);
-//       throw new Error(e);
-//     }
-//   },
-// };
-
-export const CounterActions = {};
+      commit('SET_COUNT', res.data.count);
+      commit('SET_DECREMENT_PENDING', false);
+    } catch (e) {
+      commit('SET_DECREMENT_PENDING', false);
+      throw new Error(e);
+    }
+  },
+};
 
 export default CounterActions;
