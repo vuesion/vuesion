a94d0f171a19e6e7a9414b96a6b1ed63cce9d0c7
Thomas Misikoff <tmisikoff@gmail.com>
2020-06-16 12:03:04 -0500

    feat: got routes and auth working

---
+++ src/middleware/auth.ts
@@ -0,0 +1,10 @@
+import { Middleware } from '@nuxt/types';
+
+const authMiddleware: Middleware = ({ app, store, redirect }) => {
+  if (!store.getters['auth/isAuthenticated']) {
+    const redirectArg = '?redirect=' + encodeURI(app.router.history.current.path);
+    return redirect(app.localePath('/') + redirectArg);
+  }
+};
+
+export default authMiddleware;
