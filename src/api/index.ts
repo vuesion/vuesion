ff0ca593c4101787eddd989cbef99f281d5dbdc9
Thomas Misikoff <tmisikoff@gmail.com>
2020-06-17 09:54:27 -0500

    feat: added cookie-parser, compression, and helmet

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,17 @@
 import * as express from 'express';
-import { CounterRoutes } from './routes/CounterRoutes';
+import helmet from 'helmet';
+import * as cookieParser from 'cookie-parser';
+import * as compression from 'compression';
 import { DemoRoutes } from './routes/DemoRoutes';
+import { CounterRoutes } from './routes/CounterRoutes';
 
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(helmet());
+app.use(compression({ threshold: 0 }));
 
 CounterRoutes(app);
 DemoRoutes(app);
