40176e07c6fe76ce00983c87e68606fd91583269
Thomas Misikoff <tmisikoff@gmail.com>
2020-06-18 15:08:26 -0500

    style: minor linting

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -2,7 +2,7 @@ import { Middleware } from '@nuxt/types';
 
 const authMiddleware: Middleware = ({ app, store, redirect }) => {
   if (!store.getters['auth/isAuthenticated']) {
-    const redirectArg = '?redirect=' + encodeURI(app.router.history.current.path);
+    const redirectArg = '?redirect=' + encodeURI((app.router as any).history.current.path);
     return redirect(app.localePath('/') + redirectArg);
   }
 };
