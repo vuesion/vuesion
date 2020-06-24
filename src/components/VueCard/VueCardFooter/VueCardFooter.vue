452e6e4b21bc1dfd4da952323181b52b2a1b21e0
Thomas Misikoff <tmisikoff@gmail.com>
2020-06-24 12:54:10 -0500

    fix: minor import fixes

--- src/components/VueCard/VueCardFooter/VueCardFooter.vue
+++ src/components/VueCard/VueCardFooter/VueCardFooter.vue
@@ -11,7 +11,7 @@ export default {
 </script>
 
 <style lang="scss" module>
-@import '@/assets/design-system';
+@import '~@/assets/design-system';
 
 .vueCardFooter {
   display: block;
--- src/components/VueDataTable/VueDataTableHeader/VueDataTableHeader.vue
+++ src/components/VueDataTable/VueDataTableHeader/VueDataTableHeader.vue
@@ -52,7 +52,7 @@ export default {
 </script>
 
 <style lang="scss" module>
-@import '@/assets/design-system';
+@import '~@/assets/design-system';
 
 .vueDataTableHeader {
   border: $data-table-header-border;
