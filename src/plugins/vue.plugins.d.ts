6544678a6c4ddf7d6b0c3ca86b089e59de3c8bfd
Johannes Werner <johannes.werner@hey.com>
2020-07-04 16:29:58 +0200

    feat: add vuex-persist

--- src/plugins/vue.plugins.d.ts
+++ src/plugins/vue.plugins.d.ts
@@ -1,26 +1,2 @@
-// import Vue from 'vue';
-// import { DefaultProps, PropsDefinition } from 'vue/types/options';
-// import { VeeValidateComponentOptions } from 'vee-validate';
 declare module 'vee-validate';
 declare module 'vee-validate/*';
-
-// declare module 'vue/types/vue' {
-//   interface Vue {
-//     $_veeValidate?: VeeValidateComponentOptions;
-//   }
-
-//   interface VueConstructor {
-//     prefetch?: (options: IPrefetch) => Promise<any>;
-//     $_veeValidate?: VeeValidateComponentOptions;
-//   }
-// }
-
-// declare module 'vue/types/options' {
-//   interface ComponentOptions<V extends Vue> {
-//     $_veeValidate?: VeeValidateComponentOptions;
-//   }
-
-//   interface FunctionalComponentOptions<Props = DefaultProps, PropDefs = PropsDefinition<Props>> {
-//     $_veeValidate?: VeeValidateComponentOptions;
-//   }
-// }
--- src/static/README.md
+++
@@ -1,6 +0,0 @@
-# STATIC
-
-This directory contains your static files.
-Each file inside this directory is mapped to /.
-
-Example: /static/robots.txt is mapped as /robots.txt.
