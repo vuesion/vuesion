0ff68db004b4237b3789aac28ced5b9fa4767b8e
Johannes Werner <johannes.werner@hey.com>
2020-07-12 01:46:44 +0200

    ci: fix Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,19 @@
 # build happens in the CI pipeline
 # no build inside this container because it would be redundant
 
-FROM node:12.6.0-alpine
+FROM node:12.11.0-alpine
 ENV NODE_ENV=production
-ENV PORT=3000
-RUN mkdir /logs
-COPY ./package* ./
-COPY ./nuxt ./nuxt
-COPY ./.vuesion/config.json ./.vuesion/config.json
+ENV HOST=0.0.0.0
+WORKDIR app
+COPY ./.nuxt ./.nuxt
+COPY ./.vuesion ./.vuesion
 COPY ./i18n ./i18n
+COPY ./src ./src
 COPY ./storybook-static ./storybook-static
+COPY ./nuxt.config.ts ./
+COPY ./package*.json ./
+COPY ./tsconfig.json ./
+RUN ls -la
 RUN npm install --silent --only=production
 EXPOSE 3000
 CMD npm start
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,44 +1,36 @@
 import { Configuration } from '@nuxt/types';
 
 const config: Configuration = {
-  mode: 'universal',
-  srcDir: 'src',
+  build: {
+    extractCSS: true,
+    loaders: {
+      cssModules: {
+        modules: {
+          localIdentName: '[local]_[hash:base64:4]',
+        },
+      },
+    },
+    postcss: {
+      preset: {
+        features: {
+          customProperties: false,
+        },
+      },
+    },
+    transpile: ['vee-validate'],
+  },
+  buildModules: ['@nuxt/typescript-build'],
+  css: [],
   head: {
     title: process.env.npm_package_name || '',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      {
-        hid: 'description',
-        name: 'description',
-        content: process.env.npm_package_description || '',
-      },
-    ],
+    meta: [{ charset: 'utf-8' }, { name: 'viewport', content: 'width=device-width, initial-scale=1' }],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Inter:200,400,600&display=swap' },
     ],
   },
-  loading: { color: '#cd235b' },
-  css: [],
-  plugins: [
-    { src: '~/plugins/vuex-persist/vuex-persist.client' },
-    { src: '~/plugins/vuex-persist/vuex-persist.server' },
-    { src: '~/plugins/app-config/app-config.server' },
-    { src: '~/plugins/http-service/http-service' },
-    { src: '~/plugins/vue-composition-api/vue-composition-api' },
-    { src: '~/plugins/vee-validate/vee-validate' },
-  ],
-  buildModules: ['@nuxt/typescript-build'],
-  modules: [
-    // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios',
-    '@nuxtjs/pwa',
-    'nuxt-i18n',
-  ],
-  axios: {},
   i18n: {
-    vueI18n: '~/plugins/vue-i18n/vue-i18n',
+    vueI18n: '@/plugins/vue-i18n/vue-i18n',
     locales: [
       {
         code: 'en',
@@ -74,27 +66,30 @@ const config: Configuration = {
       syncRouteParams: true,
     },
   },
-  build: {
-    extractCSS: true,
-    loaders: {
-      cssModules: {
-        modules: {
-          localIdentName: '[local]_[hash:base64:4]',
-        },
-      },
-    },
-    postcss: {
-      preset: {
-        features: {
-          customProperties: false,
-        },
-      },
-    },
-    transpile: ['vee-validate'],
+  loading: { color: '#cd235b' },
+  loadingIndicator: {
+    name: 'circle',
+    color: '#cd235b',
+    background: 'white',
   },
-  serverMiddleware: ['~/api/index.ts'],
+  mode: 'universal',
+  modules: [
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios',
+    '@nuxtjs/pwa',
+    'nuxt-i18n',
+  ],
+  plugins: [
+    { src: '@/plugins/vuex-persist/vuex-persist.client' },
+    { src: '@/plugins/vuex-persist/vuex-persist.server' },
+    { src: '@/plugins/app-config/app-config.server' },
+    { src: '@/plugins/http-service/http-service' },
+    { src: '@/plugins/vue-composition-api/vue-composition-api' },
+    { src: '@/plugins/vee-validate/vee-validate' },
+  ],
+  srcDir: 'src',
+  serverMiddleware: ['@/api/index.ts'],
   telemetry: false,
-  // extend(config, ctx) {},
 };
 
 export default config;
