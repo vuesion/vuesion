861e6e25a1b9be2c2e48c99e029dafa0f08f9e32
Johannes Werner <johannes.werner@hey.com>
2021-05-08 12:45:48 +0200

    feat(i18n): add formatFallbackMessages

--- src/plugins/vue-i18n/vue-i18n.ts
+++ src/plugins/vue-i18n/vue-i18n.ts
@@ -1,4 +1,5 @@
 import VueI18n from 'vue-i18n';
+const VuesionConfig = require('../../../.vuesion/config.json');
 
 export const calendarFormats: any = {
   calendarHeader: {
@@ -22,13 +23,21 @@ export const calendarFormats: any = {
     day: '2-digit',
   },
 };
+export const decimalFormats: any = {};
 export const dateTimeFormats: VueI18n.DateTimeFormats = {
   en: calendarFormats,
   de: calendarFormats,
+};
+export const numberFormats: VueI18n.NumberFormats = {
+  en: decimalFormats,
+  de: decimalFormats,
 };
 
 export default (): any => {
   return {
     dateTimeFormats,
+    numberFormats,
+    fallbackLocale: VuesionConfig.i18n.defaultLocale,
+    formatFallbackMessages: true,
   };
 };
