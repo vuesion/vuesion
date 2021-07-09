dfc6aa09e63b26df6458cb6eb8d3cde312b37dab
Johannes Werner <johannes.werner@hey.com>
2021-07-09 22:06:22 +0200

    feat(vue-toggle): apply re-design, update tests and stories

---
+++ src/components/input-and-actions/VueToggle/VueToggle.stories.ts
@@ -0,0 +1,70 @@
+import { storiesOf } from '@storybook/vue';
+import { action } from '@storybook/addon-actions';
+import ComponentDocs from '@/assets/design-system/docs/components/ComponentDocs.vue';
+import VueInline from '@/components/layout/VueInline/VueInline.vue';
+import VueText from '@/components/typography/VueText/VueText.vue';
+import VueToggle from './VueToggle.vue';
+
+const story = storiesOf('Input & Actions|Toggle', module) as any;
+
+story.add(
+  'Default',
+  () => ({
+    components: { VueToggle, ComponentDocs, VueInline, VueText },
+    data() {
+      return {
+        model: false,
+      };
+    },
+    template: `<component-docs
+      component-name="Toggle"
+      usage="Select lets users choose one option from an options menu.
+      Consider select when you have 4 or more options, to avoid cluttering the interface.
+      Different from dropdown menu because it is used for data entry in forms."
+      story="Show default toggle. Please interact with the toggle to see different states."
+    >
+    <vue-inline stack-phone stack-tablet-portrait stack-tablet-landscape stack-small-desktop stack-large-desktop>
+      <vue-text look="small-title" weight="semi-bold">Model: {{ model }}</vue-text>
+      <vue-toggle label="Toggle enabled" name="toggle" id="toggle" @click="action" v-model="model" />
+      <vue-toggle disabled label="Toggle disabled" name="toggle" id="toggle" @click="action" v-model="model" />
+    </vue-inline>
+    </component-docs>`,
+    methods: {
+      action: action('@onClick'),
+    },
+  }),
+  {
+    info: {
+      components: { VueToggle },
+    },
+  },
+);
+
+story.add(
+  'With description',
+  () => ({
+    components: { VueToggle, ComponentDocs, VueInline, VueText },
+    data() {
+      return {
+        model: false,
+      };
+    },
+    template: `<component-docs
+      story="Show toggle with additional description. Please interact with the toggle to see different states."
+    >
+    <vue-inline stack-phone stack-tablet-portrait stack-tablet-landscape stack-small-desktop stack-large-desktop>
+      <vue-text look="small-title" weight="semi-bold">Model: {{ model }}</vue-text>
+      <vue-toggle label="Toggle enabled" description="Get notified when someone comments on your posting." name="toggle" id="toggle" @click="action" v-model="model" />
+      <vue-toggle disabled label="Toggle disabled" description="Get notified when someone comments on your posting." name="toggle" id="toggle" @click="action" v-model="model" />
+    </vue-inline>
+</component-docs>`,
+    methods: {
+      action: action('@onClick'),
+    },
+  }),
+  {
+    info: {
+      components: { VueToggle },
+    },
+  },
+);
