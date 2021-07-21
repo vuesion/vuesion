e64bec951ad6745759000c90f892963940d10178
Johannes Werner <johannes.werner@hey.com>
2021-07-21 22:57:28 +0200

    feat(vue-avatar): apply re-design, update tests and stories

---
+++ src/components/data-display/VueAvatar/VueAvatar.stories.ts
@@ -0,0 +1,66 @@
+import { storiesOf } from '@storybook/vue';
+import ComponentDocs from '@/assets/design-system/docs/components/ComponentDocs.vue';
+import { IItem } from '@/interfaces/IItem';
+import VueStack from '@/components/layout/VueStack/VueStack.vue';
+import VueTiles from '@/components/layout/VueTiles/VueTiles.vue';
+import VueInput from '@/components/input-and-actions/VueInput/VueInput.vue';
+import VueSelect from '@/components/input-and-actions/VueSelect/VueSelect.vue';
+import VueAvatar from './VueAvatar.vue';
+
+const story = storiesOf('Data Display|Avatar', module) as any;
+// TODO: add usage
+story.add(
+  'Default',
+  () => ({
+    components: { VueAvatar, ComponentDocs, VueStack, VueTiles, VueInput, VueSelect },
+    data(): { name: string; src: IItem; size: IItem } {
+      return {
+        name: 'Isabella-Maria Del Campo',
+        src: { label: 'No src', value: null },
+        size: { label: 'Small', value: 'sm' },
+      };
+    },
+    template: `<component-docs
+      component-name="Avatar"
+      usage="TBD"
+      story="Show avatar and all its properties."
+    >
+      <vue-stack>
+        <vue-tiles :columns="[1, 2]">
+          <vue-input label="Name" name="name" id="name" hide-description v-model="name" />
+
+          <vue-select
+            :items="[
+                { label: 'No src', value: null },
+                { label: 'Stock photo', value: 'https://images.unsplash.com/photo-1607746882042-944635dfe10e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3150&q=80' },
+                ]"
+            label="Src"
+            name="src"
+            id="src"
+            hide-description
+            v-model="src"
+          />
+
+          <vue-select
+            :items="[
+                { label: 'Small', value: 'sm' },
+                { label: 'Medium', value: 'md' },
+                { label: 'Large', value: 'lg' },
+                ]"
+            label="Size"
+            name="size"
+            id="size"
+            hide-description
+            v-model="size"
+          />
+        </vue-tiles>
+        <vue-avatar :name="name" :src="src.value" :size="size.value" />
+      </vue-stack>
+    </component-docs>`,
+  }),
+  {
+    info: {
+      components: { VueAvatar },
+    },
+  },
+);
