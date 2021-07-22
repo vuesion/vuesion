bef690bd345be6a618bda253cee3f46433d95890
Johannes Werner <johannes.werner@hey.com>
2021-07-22 02:20:37 +0200

    feat(vue-tab-group): apply re-design, update tests and stories

---
+++ src/components/data-display/VueTabGroup/VueTabGroup.stories.ts
@@ -0,0 +1,49 @@
+import { storiesOf } from '@storybook/vue';
+import ComponentDocs from '@/assets/design-system/docs/components/ComponentDocs.vue';
+import VueTabGroup from './VueTabGroup.vue';
+import VueTabItem from './VueTabItem.vue';
+
+const story = storiesOf('Data Display|TabGroup', module) as any;
+// TODO: add usage
+story.add(
+  'Default',
+  () => ({
+    components: {
+      VueTabGroup,
+      VueTabItem,
+      ComponentDocs,
+    },
+    template: `<component-docs
+      component-name="TabGroup"
+      usage="TBD"
+      story="Display tabs."
+    >
+    <vue-tab-group>
+      <vue-tab-item name="Profile" icon="github">
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
+        labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
+        et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
+        labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
+        et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+      </vue-tab-item>
+      <vue-tab-item name="Settings" :is-active="true">
+        et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
+        labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
+        et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+      </vue-tab-item>
+      <vue-tab-item name="Upload" icon="arrow-up">
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
+        labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
+        et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+      </vue-tab-item>
+    </vue-tab-group>
+    </component-docs>`,
+  }),
+  {
+    info: {
+      components: { VueTabGroup, VueTabItem },
+    },
+  },
+);
---
+++ src/components/data-display/VueTabGroup/VueTabGroup.vue
@@ -0,0 +1,127 @@
+<template>
+  <div :class="$style.vueTabGroup">
+    <ul :class="$style.tabs" role="tablist">
+      <li
+        v-for="tab in tabHeader"
+        :key="tab.idx"
+        tabindex="0"
+        :class="currentTab === tab.idx ? $style.active : ''"
+        role="tab"
+        @click="changeTab(tab.idx)"
+        @keypress.space.enter.stop.prevent="changeTab(tab.idx)"
+      >
+        <component :is="`vue-icon-${tab.icon}`" v-if="tab.icon" />
+        <vue-text weight="semi-bold">
+          {{ tab.name }}
+        </vue-text>
+      </li>
+    </ul>
+
+    <vue-box padding="16">
+      <slot />
+    </vue-box>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue } from 'vue/types/vue';
+import { defineComponent, ref, provide, onBeforeUnmount } from '@vue/composition-api';
+import VueText from '@/components/typography/VueText/VueText.vue';
+import VueBox from '@/components/layout/VueBox/VueBox.vue';
+
+export default defineComponent({
+  name: 'VueTabGroup',
+  components: { VueBox, VueText },
+  setup() {
+    const tabs = ref<Array<Vue>>([]);
+    const currentTab = ref(0);
+    const tabHeader = ref<Array<{ idx: number; name: string; icon: string }>>([]);
+    const changeTab = (idx: number) => {
+      currentTab.value = idx;
+      handleTabs();
+    };
+    const handleTabs = () => {
+      tabs.value.forEach((tab: any) => {
+        tab.$data.active = tab.$data.idx === currentTab.value;
+      });
+    };
+    const register = (tab: any) => {
+      tab.$data.idx = tabs.value.length;
+
+      if (tab.$data.active) {
+        currentTab.value = tab.$data.idx;
+      }
+
+      tabs.value.push(tab);
+      tabHeader.value.push({
+        idx: tab.$data.idx,
+        name: tab.$props.name,
+        icon: tab.$props.icon,
+      });
+
+      handleTabs();
+    };
+
+    provide('register', register);
+
+    onBeforeUnmount(() => {
+      tabs.value = [];
+      tabHeader.value = [];
+      currentTab.value = 0;
+    });
+
+    return {
+      currentTab,
+      tabHeader,
+      changeTab,
+    };
+  },
+});
+</script>
+
+<style lang="scss" module>
+@import '~@/assets/_design-system';
+
+.vueTabGroup {
+  .tabs {
+    padding: 0;
+    margin: 0;
+    list-style: none;
+    display: flex;
+    flex-direction: row;
+
+    i {
+      width: $tab-icon-size;
+      height: $tab-icon-size;
+      margin-right: $tab-icon-gap;
+    }
+
+    li {
+      flex: 1 1 $tab-width;
+      display: inline-flex;
+      height: $tab-height;
+      justify-content: center;
+      align-items: flex-start;
+      padding-top: $space-4;
+      border-bottom: $tab-border;
+      color: $tab-color;
+      cursor: pointer;
+
+      &:hover {
+        border-bottom: $tab-border-hover;
+        color: $tab-color-hover;
+      }
+
+      &.active {
+        border-bottom: $tab-border-active;
+        color: $tab-color-active;
+
+        &:hover {
+          border-bottom: $tab-border-active-hover;
+          color: $tab-color-active-hover;
+        }
+      }
+    }
+  }
+}
+</style>
