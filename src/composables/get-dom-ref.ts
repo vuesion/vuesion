1dc02c9a1c8800ded93b49128d8c6388b9616594
Johannes Werner <johannes.werner@hey.com>
2021-07-26 19:17:53 +0200

    feat(get-dom-ref): add vue components as type

--- src/composables/get-dom-ref.ts
+++ src/composables/get-dom-ref.ts
@@ -1,3 +1,4 @@
+import Vue from 'vue';
 import { ref } from '@vue/composition-api';
 
-export const getDomRef = (element: HTMLElement) => ref<HTMLElement | any>(element);
+export const getDomRef = (element: HTMLElement) => ref<HTMLElement | Element | Vue | any>(element);
