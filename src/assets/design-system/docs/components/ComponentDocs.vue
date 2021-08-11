8de76dd82fcbbca29cf105b6f687c1bf62d3cc9f
Johannes Werner <johannes.werner@hey.com>
2021-08-11 02:26:14 +0200

    feat(component-docs): add vuesion+ disclaimer

--- src/assets/design-system/docs/components/ComponentDocs.vue
+++ src/assets/design-system/docs/components/ComponentDocs.vue
@@ -1,6 +1,32 @@
 <template>
   <vue-box padding="16">
     <vue-stack space="32">
+      <vue-card v-if="showDisclaimer" :class="$style.disclaimer">
+        <vue-stack>
+          <vue-text look="medium-title" color="info" weight="semi-bold"> This is a Vuesion+ component </vue-text>
+
+          <vue-text look="description" color="info">
+            We hope, that we can offer you to buy a license for this component soon.
+            <br />
+            That will save the future of this project, bring a lot of new components and save you a lot of time down the
+            road.
+          </vue-text>
+
+          <vue-text look="description" color="info">
+            We are currently busy figuring out the legal part before we can start selling.
+            <br />
+            If you know which License to choose, how to avoid liability issues and you want to help us - please reach
+            out to
+            <a href="mailto:johannes.werner@hey.com">Johannes Werner</a>.
+          </vue-text>
+
+          <vue-text look="description" color="info" weight="semi-bold">
+            If you want to get early access
+            <a href="mailto:johannes.werner@hey.com">write us an email</a>!
+          </vue-text>
+        </vue-stack>
+      </vue-card>
+
       <vue-stack v-if="componentName && usage" space="8">
         <vue-text color="text-high" look="large-title" weight="semi-bold">{{ componentName }} {{ suffix }}</vue-text>
         <vue-text color="text-high" look="description">{{ usage }}</vue-text>
@@ -33,6 +59,16 @@ export default {
     usage: { type: String, default: null },
     story: { type: String, default: null },
     suffix: { type: String, default: 'Usage' },
+    showDisclaimer: { type: Boolean, default: false },
   },
 };
 </script>
+
+<style lang="scss" module>
+@import '~@/assets/_design-system';
+
+.disclaimer {
+  background: var(--brand-surface-info-low);
+  border: 1px solid var(--brand-border-info-low);
+}
+</style>
