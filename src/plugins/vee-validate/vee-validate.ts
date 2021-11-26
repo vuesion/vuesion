6fd6b3aafb7c188ae57a8c88a2dba2e589356da5
Johannes Werner <johannes.werner@hey.com>
2021-11-26 16:20:38 +0100

    feat(vee-validate): add regex by default

--- src/plugins/vee-validate/vee-validate.ts
+++ src/plugins/vee-validate/vee-validate.ts
@@ -1,7 +1,8 @@
 import { extend } from 'vee-validate';
-import { required, email, integer, min } from 'vee-validate/dist/rules.umd.js';
+import { required, email, integer, min, regex } from 'vee-validate/dist/rules.umd.js';
 
 extend('required', required);
 extend('email', email);
 extend('integer', integer);
 extend('min', min);
+extend('regex', regex);
