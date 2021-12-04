c30280143e7f576b869a30bc0fb0bd5f28f9f997
Johannes Werner <johannes.werner@hey.com>
2021-12-04 15:28:14 +0100

    feat(color-palette): add info color

--- src/assets/design-system/docs/components/ColorPalette.vue
+++ src/assets/design-system/docs/components/ColorPalette.vue
@@ -84,28 +84,6 @@ export default {
   neutral-neutral-9: palette-color-level('neutral', 9);
   neutral-neutral-10: palette-color-level('neutral', 10);
 
-  danger-danger-1: palette-color-level('danger', 1);
-  danger-danger-2: palette-color-level('danger', 2);
-  danger-danger-3: palette-color-level('danger', 3);
-  danger-danger-4: palette-color-level('danger', 4);
-  danger-danger-5: palette-color-level('danger', 5);
-  danger-danger-6: palette-color-level('danger', 6);
-  danger-danger-7: palette-color-level('danger', 7);
-  danger-danger-8: palette-color-level('danger', 8);
-  danger-danger-9: palette-color-level('danger', 9);
-  danger-danger-10: palette-color-level('danger', 10);
-
-  warning-warning-1: palette-color-level('warning', 1);
-  warning-warning-2: palette-color-level('warning', 2);
-  warning-warning-3: palette-color-level('warning', 3);
-  warning-warning-4: palette-color-level('warning', 4);
-  warning-warning-5: palette-color-level('warning', 5);
-  warning-warning-6: palette-color-level('warning', 6);
-  warning-warning-7: palette-color-level('warning', 7);
-  warning-warning-8: palette-color-level('warning', 8);
-  warning-warning-9: palette-color-level('warning', 9);
-  warning-warning-10: palette-color-level('warning', 10);
-
   success-success-1: palette-color-level('success', 1);
   success-success-2: palette-color-level('success', 2);
   success-success-3: palette-color-level('success', 3);
@@ -127,5 +105,27 @@ export default {
   info-info-8: palette-color-level('info', 8);
   info-info-9: palette-color-level('info', 9);
   info-info-10: palette-color-level('info', 10);
+
+  warning-warning-1: palette-color-level('warning', 1);
+  warning-warning-2: palette-color-level('warning', 2);
+  warning-warning-3: palette-color-level('warning', 3);
+  warning-warning-4: palette-color-level('warning', 4);
+  warning-warning-5: palette-color-level('warning', 5);
+  warning-warning-6: palette-color-level('warning', 6);
+  warning-warning-7: palette-color-level('warning', 7);
+  warning-warning-8: palette-color-level('warning', 8);
+  warning-warning-9: palette-color-level('warning', 9);
+  warning-warning-10: palette-color-level('warning', 10);
+
+  danger-danger-1: palette-color-level('danger', 1);
+  danger-danger-2: palette-color-level('danger', 2);
+  danger-danger-3: palette-color-level('danger', 3);
+  danger-danger-4: palette-color-level('danger', 4);
+  danger-danger-5: palette-color-level('danger', 5);
+  danger-danger-6: palette-color-level('danger', 6);
+  danger-danger-7: palette-color-level('danger', 7);
+  danger-danger-8: palette-color-level('danger', 8);
+  danger-danger-9: palette-color-level('danger', 9);
+  danger-danger-10: palette-color-level('danger', 10);
 }
 </style>
