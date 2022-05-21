e5d550c8c1f754c4a75a81d5f7afa1b5fd4e7f33
Johannes Werner <johannes.werner@hey.com>
2022-05-21 22:57:36 +0200

    feat(vee-validate): add min_value rule

--- src/plugins/vee-validate/vee-validate.ts
+++ src/plugins/vee-validate/vee-validate.ts
@@ -1,8 +1,9 @@
 import { extend } from 'vee-validate';
-import { required, email, integer, min, regex } from 'vee-validate/dist/rules.umd.js';
+import { required, email, integer, min, min_value as minValue, regex } from 'vee-validate/dist/rules.umd.js';
 
 extend('required', required);
 extend('email', email);
 extend('integer', integer);
 extend('min', min);
+extend('min_value', minValue);
 extend('regex', regex);
