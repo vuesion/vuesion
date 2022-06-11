a20f752f944032f4628a77a409669c92e36012a5
Johannes Werner <johannes.werner@hey.com>
2022-06-11 22:11:05 +0200

    feat(components): add vue-hidden

---
+++ src/components/utils/VueHidden/VueHidden.stories.ts
@@ -0,0 +1,34 @@
+import VueHidden from './VueHidden.vue';
+import ComponentDocs from '@/assets/design-system/docs/components/ComponentDocs.vue';
+
+export default {
+  title: 'Foundation/Utils',
+  component: VueHidden,
+};
+
+const Template = (args) => ({
+  components: { VueHidden, ComponentDocs },
+  setup() {
+    return { args };
+  },
+  template: `<component-docs
+      component-name="Hidden"
+      usage="Use to hide content in different viewports."
+  >
+  <vue-hidden v-bind="args">
+    Text content, HTML elements or components go here.
+  </vue-hidden>
+  </component-docs>`,
+});
+
+export const Hidden = Template.bind({});
+
+Hidden.args = {
+  as: 'div',
+  phone: false,
+  tabletPortrait: false,
+  tabletLandscape: false,
+  smallDesktop: false,
+  largeDesktop: false,
+  onScreen: false,
+};
