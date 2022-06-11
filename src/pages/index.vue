d00972c32ff3ac0fa6a07de36ea0501d39476fd3
Johannes Werner <johannes.werner@hey.com>
2022-06-11 22:27:38 +0200

    chore: update deps

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -8,12 +8,7 @@
   </vue-stack>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import VueText from '@/components/typography/VueText/VueText.vue';
-import VueBox from '@/components/layout/VueBox/VueBox.vue';
 import VueStack from '@/components/layout/VueStack/VueStack.vue';
-
-export default {
-  components: { VueStack, VueBox, VueText },
-};
 </script>
