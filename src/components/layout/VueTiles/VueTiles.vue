131220793a59b826671631ca19b7ee9cc49b642e
Johannes Werner <johannes.werner@hey.com>
2024-05-30 18:04:45 +0200

    refactor(vue-tiles): inherit from vue-box

--- src/components/layout/VueTiles/VueTiles.vue
+++ src/components/layout/VueTiles/VueTiles.vue
@@ -1,18 +1,20 @@
 <template>
-  <component :is="as" :class="cssClasses">
+  <vue-box :as="as" :padding="padding" :class="cssClasses">
     <slot />
-  </component>
+  </vue-box>
 </template>
 
 <script setup lang="ts">
 import { computed } from 'vue';
-import type { Spacing } from '~/components/prop-types';
+import type { Spacing, SpacingWithDirections } from '~/components/prop-types';
 import { getResponsiveCssClasses, parseResponsivePropValue } from '~/components/utils';
+import VueBox from '~/components/layout/VueBox/VueBox.vue';
 
 type ColumnRange = 'none' | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
 
 interface TilesProps {
   as?: string;
+  padding?: SpacingWithDirections | Array<SpacingWithDirections>;
   space?: Spacing | Array<Spacing>;
   verticalSpace?: Spacing | Array<Spacing> | null;
   horizontalSpace?: Spacing | Array<Spacing> | null;
@@ -21,6 +23,7 @@ interface TilesProps {
 
 const props = withDefaults(defineProps<TilesProps>(), {
   as: 'div',
+  padding: () => [0],
   space: () => [24],
   verticalSpace: null,
   horizontalSpace: null,
