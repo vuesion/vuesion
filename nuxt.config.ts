8b4621f63cea42fd9bbb3e17f1964b46dc3426d0
Johannes Werner <johannes.werner@hey.com>
2025-11-22 12:03:38 +0100

    chore(dependencies): update nuxt/color-mode to v4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -43,7 +43,6 @@ export default defineNuxtConfig({
   colorMode: {
     preference: 'system',
     fallback: 'light',
-    hid: 'nuxt-color-mode-script',
     globalName: '__NUXT_COLOR_MODE__',
     componentName: 'ColorScheme',
     classPrefix: '',
